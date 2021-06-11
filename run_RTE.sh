aba52d7f3dcf06b1cdc43e79224363ba69c68660
areikka <zhangjingao80@163.com>
2021-06-11 08:04:47 +0100

    run on ipu

--- run_RTE.sh
+++ run_RTE.sh
@@ -2,7 +2,7 @@ export TASK_NAME=rte
 # export POPLAR_ENGINE_OPTIONS='{"autoReport.directory":"./profiles/sec_try","autoReport.outputExecutionProfile":"true", "debug.allowOutOfMemory": "true", "autoReport.outputSerializedGraph": "false", "debug.outputAllSymbols": "true", "autoReport.all": "true"}'
 export POPLAR_ENGINE_OPTIONS='{"debug.allowOutOfMemory": "true", "autoReport.outputSerializedGraph": "false", "debug.outputAllSymbols": "true", "autoReport.all": "true","autoReport.directory":"./profiles/3rd_try","profiler.perExecutionStreamCopyCycles":"true"}'
 
-python run_glue_no_trainer.py \
+python run_glue_no_trainer_ipu.py \
   --model_name_or_path bart-base \
   --task_name $TASK_NAME \
   --max_length 128 \
